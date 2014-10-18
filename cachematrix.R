98f254f323a4b3375e5766dffed800c084662fc6
Mattex84 <matteo.marco.deiana@gmail.com>
2014-10-18 23:42:15 +0200

    Assignment2_mattex84
    Defining functions for handle a cached reverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function define a list of function that allows to store in cache the inverse of a matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+		m <- NULL
+		
+		## Define a function that store a matrix in the cache.
+		set <- function(y)
+		{
+			x <<- y
+			m <<- NULL		
+		}
+		
+		## Define a function that return a matrix
+		get <-  function() 
+		{
+			x
+		}
+		
+		## Define a function that store a inverse of matrix in the cache.
+		setInverse <- function(z)
+		{
+			m <<- z	
+		}
+		
+		## Define a function that return the inverse of a matriche from the cache
+		getInverse <-  function()
+		{
+			m
+		}
+		
+		list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+		
 }
 
 
-## Write a short comment describing this function
+## This function check if the inverse of a matrix is already cached: 
+## if YES, the function only returned the stored matrix
+## if NO the funcion put the inverse of the matrix in the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+        ## Try to obtain the Reverse matrix
+        y <- x$getInverse
+        
+        ## If the reverse matrix is already cached it is simply returned
+        if(!is.na(y))
+        {
+        	message("loading cached data")
+        	return(y)
+        }
+        ## If the reverse in not cached it is chached and than returned.
+        else
+        {
+        	park <- x$get()
+        	z <- solve(park)
+        	x$setInverse(z)
+        	z
+        }
+        
 }
